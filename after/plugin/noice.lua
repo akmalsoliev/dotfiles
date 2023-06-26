14f1318c6b9f76a87ee4ccbd02ab80859c0b866e
Akmal Soliev <soliev@gmail.com>
2023-06-26 14:22:38 +0200

    Noice - only cmd and docs

--- after/plugin/noice.lua
+++ after/plugin/noice.lua
@@ -2,14 +2,14 @@ require("noice").setup({
   lsp = {
     -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
     override = {
-      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-      ["vim.lsp.util.stylize_markdown"] = true,
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = false,
+      ["vim.lsp.util.stylize_markdown"] = false,
       ["cmp.entry.get_documentation"] = true,
     },
   },
   -- you can enable a preset for easier configuration
   presets = {
-    bottom_search = true, -- use a classic bottom cmdline for search
+    bottom_search = false, -- use a classic bottom cmdline for search
     command_palette = true, -- position the cmdline and popupmenu together
     long_message_to_split = true, -- long messages will be sent to a split
     inc_rename = false, -- enables an input dialog for inc-rename.nvim
@@ -21,5 +21,8 @@ require("noice").setup({
   },
   notify = {
     enabled = false
-  }
+  },
+  health = {
+    checker = false, -- Disable if you don't want health checks to run
+  },
 })

ca1ca3f61c39f47aef1281c45a4bf7d258509cee
Akmal Soliev <soliev@gmail.com>
2023-12-01 11:39:57 +0100

    [NEW] Added csv files rainbow

--- lua/asol/lazy.lua
+++ lua/asol/lazy.lua
@@ -135,6 +135,28 @@ local plugins = {
       -- refer to the configuration section below
     }
   },
+
+  -- CSV Rainbow
+  {
+    'cameron-wags/rainbow_csv.nvim',
+    config = true,
+    ft = {
+      'csv',
+      'tsv',
+      'csv_semicolon',
+      'csv_whitespace',
+      'csv_pipe',
+      'rfc_csv',
+      'rfc_semicolon'
+    },
+    cmd = {
+      'RainbowDelim',
+      'RainbowDelimSimple',
+      'RainbowDelimQuoted',
+      'RainbowMultiDelim',
+      'RainbowAlign',
+    }
+  }
 }
 
 local opts = {}

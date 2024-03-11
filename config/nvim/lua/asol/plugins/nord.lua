9bd6853c0b256ad151cd56bd0b65ad7a2eee3704
Akmal Soliev <soliev@gmail.com>
2024-03-11 14:55:53 +0100

    [FIX] Nord doesn't load properly sometimes

--- config/nvim/lua/asol/plugins/nord.lua
+++ config/nvim/lua/asol/plugins/nord.lua
@@ -1,11 +1,12 @@
 return {
   'shaunsingh/nord.nvim',
+  priority = 1000,
   opts = {
     nord_contrast = false,
     nord_disable_background = true,
     nord_bold = false,
   },
-  config = function() -- This is the function that runs, AFTER loading
+  config = function()
     require('nord').set()
   end
 }

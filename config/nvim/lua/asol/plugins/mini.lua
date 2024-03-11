4a89c7916410125f472c3c3284aac5ee0e83193e
Akmal Soliev <soliev@gmail.com>
2024-03-11 14:55:34 +0100

    [NEW] Setting keymaps

--- config/nvim/lua/asol/plugins/mini.lua
+++ config/nvim/lua/asol/plugins/mini.lua
@@ -2,36 +2,21 @@ return { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     config = function()
         -- Better Around/Inside textobjects
-        --
-        -- Examples:
-        --  - va)  - [V]isually select [A]round [)]paren
-        --  - yinq - [Y]ank [I]nside [N]ext [']quote
-        --  - ci'  - [C]hange [I]nside [']quote
         require('mini.ai').setup { n_lines = 500 }
 
         -- Add/delete/replace surroundings (brackets, quotes, etc.)
-        --
-        -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-        -- - sd'   - [S]urround [D]elete [']quotes
-        -- - sr)'  - [S]urround [R]eplace [)] [']
-        require('mini.surround').setup()
-
-        -- Simple and easy statusline.
-        --  You could remove this setup call if you don't like it,
-        --  and try some other statusline plugin
-        local statusline = require 'mini.statusline'
-        -- set use_icons to true if you have a Nerd Font
-        statusline.setup { use_icons = vim.g.have_nerd_font }
-
-        -- You can configure sections in the statusline by overriding their
-        -- default behavior. For example, here we set the section for
-        -- cursor location to LINE:COLUMN
-        ---@diagnostic disable-next-line: duplicate-set-field
-        statusline.section_location = function()
-            return '%2l:%-2v'
-        end
-
-        -- ... and there is more!
-        --  Check out: https://github.com/echasnovski/mini.nvim
+        require('mini.surround').setup({
+            mappings = {
+                add = '<leader>msa', -- Add surrounding in Normal and Visual modes
+                delete = '<leader>msd', -- Delete surrounding
+                find = '<leader>msf', -- Find surrounding (to the right)
+                find_left = '<leader>msF', -- Find surrounding (to the left)
+                highlight = '<leader>msh', -- Highlight surrounding
+                replace = '<leader>msr', -- Replace surrounding
+                update_n_lines = '<leader>msn', -- Update `n_lines`
+                suffix_last = '', -- Suffix to search with "prev" method
+                suffix_next = '', -- Suffix to search with "next" method
+            }
+        })
     end,
 }

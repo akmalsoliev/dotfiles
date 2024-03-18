b9839b180afa052fd120773ba9aa1f76e7f0980f
Akmal Soliev <soliev@gmail.com>
2024-03-18 12:15:32 +0100

    NEW: Autoformat and new languages

---
+++ config/nvim/lua/asol/plugins/conform.lua
@@ -0,0 +1,16 @@
+return { -- Autoformat
+	"stevearc/conform.nvim",
+	opts = {
+		notify_on_error = false,
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_fallback = true,
+		},
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "ruff" },
+			bash = { "beautysh" },
+			sql = { "sql_formatter" },
+		},
+	},
+}

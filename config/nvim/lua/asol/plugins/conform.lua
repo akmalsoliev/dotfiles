a2b033265fb6aa230ae28cea9d884a987cb8874d
Akmal Soliev <soliev@gmail.com>
2024-03-21 17:26:30 +0100

    FIX: Python formatter

--- config/nvim/lua/asol/plugins/conform.lua
+++ config/nvim/lua/asol/plugins/conform.lua
@@ -1,28 +1,33 @@
 return { -- Autoformat
   "stevearc/conform.nvim",
+  formatters = {
+    stylelua = {
+      indent_width = 2
+    }
+  },
   keys = {
     {
       "<leader>f",
       function()
-        require("conform").format({ async = true, timeout_ms = 3000 })
+        require("conform").format({ timeout_ms = 3000 })
       end,
       mode = { "n", "v" },
       desc = "[F]ormat file",
     },
   },
   opts = {
-    notify_on_error = false,
+    notify_on_error = true,
     format_on_save = {
       timeout_ms = 500,
       lsp_fallback = true,
     },
     formatters_by_ft = {
       lua = { "stylua" },
-      python = { "ruff" },
+      python = { "ruff_fix", "ruff_format" },
       bash = { "beautysh" },
       sql = { "sql_formatter" },
       json = { "prettier" },
-      ["*"] = { "trim_whitespace", "codespell" },
+      ["*"] = { "trim_whitespace" },
     },
   },
 }

72a4c50eab226f4214c7f6e400147f5dba86ede2
Akmal Soliev <soliev@gmail.com>
2024-03-24 12:01:49 +0100

    NEW: Open `lazygit`

--- config/nvim/lua/asol/plugins/fterm.lua
+++ config/nvim/lua/asol/plugins/fterm.lua
@@ -1,3 +1,26 @@
+vim.api.nvim_create_user_command(
+  'Lazygit',
+  function()
+    local Terminal = require("FTerm")
+    local lazygit = Terminal:new {
+      cmd = "lazygit",
+      hidden = true,
+      dimensions = {
+        border = "none",
+        width = 1,
+        height = 1,
+      },
+      on_open = function(_)
+        vim.cmd "startinsert!"
+      end,
+      on_close = function(_) end,
+      count = 99,
+    }
+    lazygit:toggle()
+  end,
+  { bang = true }
+)
+
 return {
   "numToStr/FTerm.nvim",
   lazy = false,

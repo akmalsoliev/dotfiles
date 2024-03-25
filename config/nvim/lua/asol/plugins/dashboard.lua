25b9849c87be0f239bef86886154ad93de34e9b7
Akmal Soliev <soliev@gmail.com>
2024-03-25 10:02:47 +0100

    NEW: Dashboard modification

--- config/nvim/lua/asol/plugins/dashboard.lua
+++ config/nvim/lua/asol/plugins/dashboard.lua
@@ -1,9 +1,24 @@
+local banner = {
+  '  /$$$$$$        /$$                                                 /$$   ',
+  ' /$$__  $$      | $$                                                | $$   ',
+  '| $$  \\ $$      | $$   /$$       /$$$$$$/$$$$         /$$$$$$       | $$  ',
+  '| $$$$$$$$      | $$  /$$/      | $$_  $$_  $$       |____  $$      | $$   ',
+  '| $$__  $$      | $$$$$$/       | $$ \\ $$ \\ $$        /$$$$$$$      | $$ ',
+  '| $$  | $$      | $$_  $$       | $$ | $$ | $$       /$$__  $$      | $$   ',
+  '| $$  | $$      | $$ \\  $$      | $$ | $$ | $$      |  $$$$$$$      | $$  ',
+  '|__/  |__/      |__/  \\__/      |__/ |__/ |__/       \\_______/      |__/ ',
+  '                                                                           ',
+}
+
 return {
   'nvimdev/dashboard-nvim',
   event = 'VimEnter',
   config = function()
     require('dashboard').setup {
-      -- config
+      theme = "hyper",
+      config = {
+        header = banner,
+      }
     }
   end,
   dependencies = { {'nvim-tree/nvim-web-devicons'}}

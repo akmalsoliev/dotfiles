729e944a5d142914f079eba78e6f59c47ef04e3f
Akmal Soliev <soliev@gmail.com>
2024-07-30 17:12:08 +0200

    FIX: #8 and format

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,70 +1,59 @@
 if status is-interactive
-
-# Neofetch
-neofetch
-
-# Activate venv
-function sv
+  # Activate virtual env and save the path as a tmux variable,
+  # so that new panes/windows can re-activate as necessary
+  if test -d .venv
     source .venv/bin/activate.fish
-    and tmux set-environment VIRTUAL_ENV $VIRTUAL_ENV
-end
-if test -n "$VIRTUAL_ENV"
-    source $VIRTUAL_ENV/bin/activate.fish
-end
+  end
 
-# iterm integration
-source ~/.iterm2_shell_integration.fish
+  # iterm integration
+  test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
-if status is-interactive
-and not set -q TMUX
+  # iterm integration
+  source ~/.iterm2_shell_integration.fish
+
+  if status is-interactive
+    and not set -q TMUX
     exec tmux
-end
+  end
 
-export PATH="/Users/$USER/.local/bin:$PATH"
+  export PATH="/Users/$USER/.local/bin:$PATH"
 
-# lsd aliases
-alias ls='lsd'
-alias lf='lsd -Fl'
-alias la='lsd -Fla'
-alias lt='lsd --tree'
-alias l1='lsd -1'
-# cli aliases
-alias clr='clear'
-# git aliases
-alias gs='git status'
-alias ga='git add'
-alias gc='git commit'
-alias gd='git diff'
-# Neovim aliase
-alias vim='nvim'
-alias ovim='nvim .'
-# upgrading all outdated pip packages
-alias pipup="pip freeze | sed 's/=.*//' | xargs pip install --upgrade"
-# uninstall all pip
-alias pipuninstall="pip3 list --format freeze | sed 's/=.*//' | xargs pip3 uninstall -y"
-# glow default theme
-alias glow="glow -s /Users/akmalsoliev/Library/Preferences/glow/nord.json"
+  # lsd aliases
+  alias ls='lsd'
+  alias lf='lsd -Fl'
+  alias la='lsd -Fla'
+  alias lt='lsd --tree'
+  alias l1='lsd -1'
 
-# Cargo aliases
-alias cr="cargo run"
-alias ca="cargo add"
-alias cc="cargo check"
+  # cli aliases
+  alias clr='clear'
 
-set -g theme_powerline_fonts no
-set -g theme_nerd_fonts yes
+  # git aliases
+  alias gs='git status'
+  alias ga='git add'
+  alias gc='git commit'
+  alias gd='git diff'
 
-# pyenv
-pyenv init - | source
+  # Neovim aliase
+  alias vim='nvim'
+  alias ovim='nvim .'
 
-# direnv
-direnv hook fish | source
+  # glow default theme
+  alias glow="glow -s /Users/akmalsoliev/Library/Preferences/glow/nord.json"
 
-# Removes date from the right side of the command line
-# intentional, because have everything in the tmux statusline
-function fish_right_prompt
-  #intentionally left blank
- end
+  set -g theme_powerline_fonts no
+  set -g theme_nerd_fonts yes
+
+  # pyenv
+  pyenv init - | source
+
+  # direnv
+  direnv hook fish | source
+
+  # Removes date from the right side of the command line
+  # intentional, because have everything in the tmux statusline
+  function fish_right_prompt
+    #intentionally left blank
+  end
 
 end
-
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish

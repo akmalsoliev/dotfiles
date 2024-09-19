5772ac7239831748134ede72ab15d7c6107f31ab
Akmal Soliev <soliev@gmail.com>
2024-09-19 11:54:32 +0200

    UPDATE: Minor additions

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -44,9 +44,6 @@ if status is-interactive
   set -g theme_powerline_fonts no
   set -g theme_nerd_fonts yes
 
-  # pyenv
-  pyenv init - | source
-
   # direnv
   direnv hook fish | source
 
@@ -57,3 +54,4 @@ if status is-interactive
   end
 
 end
+uv generate-shell-completion fish | source

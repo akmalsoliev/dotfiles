580e78471b9778649507af251c9130794f94b90b
Akmal Soliev <soliev@gmail.com>
2024-11-26 21:31:09 +0100

    NEW: Auto sessions, saves sessions

--- config/nvim/lua/asol/plugins/auto-session.lua
+++ config/nvim/lua/asol/plugins/auto-session.lua
@@ -1,12 +1,8 @@
 return {
-  "rmagatti/auto-session",
-  lazy = false,
-
-  ---enables autocomplete for opts
-  ---@module "auto-session"
-  ---@type AutoSession.Config
-  opts = {
-    suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-    -- log_level = 'debug',
+  {
+    "rmagatti/auto-session",
+    opts = {
+      auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+    },
   },
 }

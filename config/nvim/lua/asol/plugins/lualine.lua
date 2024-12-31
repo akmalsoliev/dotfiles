e7f2e1a7ab74b75274e996759288c7f1ada5bf63
Akmal Soliev <soliev@gmail.com>
2024-12-31 15:11:31 +0100

    NEW: catppuccin lualine

--- config/nvim/lua/asol/plugins/lualine.lua
+++ config/nvim/lua/asol/plugins/lualine.lua
@@ -1,45 +1,59 @@
+local colors = {
+  blue = "#80a0ff",
+  cyan = "#79dac8",
+  black = "#080808",
+  white = "#c6c6c6",
+  red = "#ff5189",
+  violet = "#d183e8",
+  grey = "#303030",
+}
+
+local bubbles_theme = {
+  normal = {
+    a = { fg = colors.black, bg = colors.violet },
+    b = { fg = colors.white, bg = colors.grey },
+    c = { fg = colors.white },
+  },
+
+  insert = { a = { fg = colors.black, bg = colors.blue } },
+  visual = { a = { fg = colors.black, bg = colors.cyan } },
+  replace = { a = { fg = colors.black, bg = colors.red } },
+
+  inactive = {
+    a = { fg = colors.white, bg = colors.black },
+    b = { fg = colors.white, bg = colors.black },
+    c = { fg = colors.white },
+  },
+}
+
 return {
-  'nvim-lualine/lualine.nvim',
+  "nvim-lualine/lualine.nvim",
   opts = {
     options = {
-      icons_enabled = true,
-      bg = '#5E81AC',
-      component_separators = { left = '', right = ''},
-      section_separators = { left = '', right = ''},
-      disabled_filetypes = {
-        statusline = {},
-        winbar = {},
-      },
-      ignore_focus = {},
-      always_divide_middle = true,
-      globalstatus = false,
-      refresh = {
-        statusline = 1000,
-        tabline = 1000,
-        winbar = 1000,
-      }
+      theme = bubbles_theme,
+      component_separators = "",
+      section_separators = { left = "", right = "" },
     },
     sections = {
-      lualine_a = {'mode'},
-      lualine_b = {'branch', 'diff', 'diagnostics'},
-      lualine_c = {'filename'},
-      lualine_x = {'encoding', 'filetype'},
-      lualine_y = {'progress'},
-      lualine_z = {}
+      lualine_a = { "mode" },
+      lualine_b = { "branch", "diff", "diagnostics" },
+      lualine_c = { "filename" },
+      lualine_x = { "filetype" },
+      lualine_y = { "progress" },
+      lualine_z = {},
     },
     inactive_sections = {
       lualine_a = {},
       lualine_b = {},
-      lualine_c = {'filename'},
-      lualine_x = {'location'},
+      lualine_c = { "filename" },
+      lualine_x = { "location" },
       lualine_y = {},
-      lualine_z = {}
+      lualine_z = {},
     },
     tabline = {},
     winbar = {},
     inactive_winbar = {},
-    extensions = {'nvim-tree'},
-    theme = 'nord',
+    extensions = { "nvim-tree" },
   },
-  dependencies = { 'nvim-tree/nvim-web-devicons', opt = true }
+  dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
 }
